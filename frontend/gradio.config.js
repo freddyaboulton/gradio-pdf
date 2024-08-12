9b1bab6082733de74c6a680a0bd6423ad5f7beca
freddyaboulton <alfonsoboulton@gmail.com>
2024-08-12 15:37:13 -0400

    Add code

--- frontend/gradio.config.js
+++ frontend/gradio.config.js
@@ -3,5 +3,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
       target: 'esnext' //browsers can handle the latest ES features
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "esnext",
+      }
     }
 })
